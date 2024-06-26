675d6be248cdab59e63a1a45bf931f6c1a79968e
vincent anjiri <vincentanjiri12@gmail.com>
2024-06-26 09:53:09 +0300

    .hello

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// const calcTempAmplitude = function (temps) {
+//   let max = temps[0];
+//   let min = temps[0];
+//   for (let i = 0; i < temps.length; i++) {
+//     const currTemp = temps[i];
+//     if (typeof currTemp !== 'number') continue;
+//     if (currTemp > max) {
+//       max = currTemp;
+//       if (currTemp < min) {
+//         min = currTemp;
+//       }
+//     }
+//     console.log(max, min);
+//     return max - min;
+//   }
+// };
+
+// const amplitude = calcTempAmplitude(temperatures);
+// console.log(amplitude);
+const arry = [17, 21, 23];
+
+function printForecast(arry) {
+  for (let i = 0; i < arry.length; i++) {
+    let maxTemp = arry[i];
+  }
+}
